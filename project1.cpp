dfcf0a75e9177ccd0da6dbc2d901a47f1b417068
isllamkhalil <118750615+isllam-colon@users.noreply.github.com>
2024-05-13 14:33:43 +0300

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,122 @@
+å#include <iostream>
+#include <string>
+using namespace std;
+#define size 100
+
+struct student
+{
+    double grade;
+    string name;
+    int id;
+};
+
+
+student Queue[size];
+int front = -1;
+int rear = -1;
+
+void enqueue(student Student)
+{
+    if(rear != size-1)
+    {
+        if(front == -1 && rear == -1)
+        {
+            front++;
+                Queue[++rear]= Student;
+        }
+        else 
+        {
+                Queue[++rear]= Student;
+        }
+    }
+    else
+    {
+        cout<<"The list is full" << endl;
+    }
+}
+
+void dequeue()
+{
+        if(front != -1 && rear != -1 && front <= rear)
+    {
+        front++;
+    }
+    else
+    {
+        cout<< "queue is empty" << endl;
+    }
+}
+
+void addStudent()
+{
+    student Student;
+    cout << "Enter student ID: ";
+    cin >> Student.id;
+    cout << "Enter student name: ";
+    cin >> Student.name ;
+    cout << "Enter student grade: ";
+    cin >> Student.grade;
+
+    enqueue(Student);
+    cout << "Student added to the queue." << endl;
+}
+
+
+double calculateGradeAverage()
+{
+    double total = 0;
+    int count = 0;
+    for (int i = front; i <= rear; ++i) 
+    {
+        total += Queue[i].grade;
+        count++;
+    }
+    if (count == 0) 
+    {
+        return 0;
+    }
+    return total / count;
+}
+
+void CalcAvgPass()
+{
+    double sum = 0, n = 0, avg;
+    for (int i = front;i <= rear; i++)
+    {
+        if (Queue[i].grade >= 50)
+        {
+            sum = sum + Queue[i].grade;
+            n++;
+        }
+        if (n == 0) 
+        {
+        cout<<"0";
+        }  
+    }
+    avg = sum/n;
+    cout<< avg;
+}
+void separatestudents()
+{
+    student passedqueue[size], failedqueue[size];
+    int passedrear = -1, failedrear=-1;
+
+    for (int i=0;i < rear ;i++)
+    if(Queue[i].grade>=50)
+        passedqueue[++passedrear]= Queue[i];
+    else
+        failedqueue[++failedrear]= Queue[i];
+} 
+
+int main()
+{
+    addStudent();
+    addStudent();
+    addStudent();
+    separatestudents();
+    cout<<"the Average of passed students : "  ;
+    CalcAvgPass();
+
+
+    return 0;
+}
---
+++ project2.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct contact
+{
+    string name;
+    string tel;
+    contact* left;
+    contact* right;
+};
+
+void addcontact(contact*& newcontact, string nme, string num)
+{
+    newcontact = new contact;
+    newcontact->name = nme;
+    newcontact->tel = num;
+    newcontact->left = nullptr;
+    newcontact->right = nullptr;
+}
+
+void adbol(contact*& root, string nme, string num)
+{
+    if(root == NULL)
+    {
+    addcontact(root, nme, num);
+    return;
+    }
+    if (nme < root->name)
+    {
+        return adbol(root->left, nme, num);
+    }
+    else
+    {
+        return adbol(root->right, nme, num);
+    }
+}
+
+void addtobook(contact*& root)
+{
+    string nme;
+    string num;
+
+    cout<<"enter a name: ";getline(cin, nme);
+    cout<<"enter it's number: ";getline(cin, num);cout<<endl;
+    cout<<"contact added! ";cout<<endl;
+
+    (adbol(root, nme, num));
+}
+
+string srchbolnum(contact*& root, string nme)
+{
+    if(root->name == nme)
+    {
+        string nom;
+        nom = root->tel;
+        return nom;
+    }
+    if (nme < root->name)
+    {
+        return srchbolnum(root->left, nme);
+    }
+    else
+    {
+        return srchbolnum(root->right, nme);
+    }
+}
+
+bool srchbol(contact*& root, string nme)
+{
+    if(root == NULL)
+    {
+        return false;
+    }
+    if(root->name == nme)
+    {
+        return true;
+    }
+    if (nme < root->name)
+    {
+        return srchbol(root->left, nme);
+    }
+    else
+    {
+        return srchbol(root->right, nme);
+    }
+}
+
+void search(contact*& root)
+{
+    string nme;
+    cout<<"enter the contact to be found: ";getline(cin, nme);
+    if (srchbol(root, nme))
+    {
+        cout<<nme<<" is found and the number is "<<srchbolnum(root, nme); 
+    }
+    else
+    {
+        cout<<nme<<" is not found";
+    }
+}
+
+
+void displayContacts(contact* root) 
+{
+    if (root == nullptr) 
+    {
+        return;
+    }
+    displayContacts(root->left);
+    cout << "Name: " << root->name << endl << "Number: " << root->tel << endl;
+    displayContacts(root->right);
+}
+
+int main()
+{
+    contact* root = nullptr;
+    addtobook(root);
+    addtobook(root);
+    addtobook(root);
+    displayContacts(root);
+
+    return 0;
+}
